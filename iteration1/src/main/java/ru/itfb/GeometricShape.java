a0f1692e329318b57b933b051157b9d532651c9b
Sergey Anisimov <sa@uniz.ru>
2024-02-21 16:15:24 +0300

    init

--- iteration1/src/main/java/ru/itfb/GeometricShape.java
+++ iteration1/src/main/java/ru/itfb/GeometricShape.java
@@ -13,22 +13,6 @@ public abstract class GeometricShape {
         this.y = y;
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public double square () {
         return x*y;
     }
--- iteration1/src/main/java/ru/itfb/Main.java
+++ iteration1/src/main/java/ru/itfb/Main.java
@@ -3,8 +3,8 @@ package ru.itfb;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    private static Rectangle rectangle = new Rectangle(3, 6);
-    private static Circle circle = new Circle(3);
+    private static final Rectangle rectangle = new Rectangle(3, 6);
+    private static final Circle circle = new Circle(3);
     public static void main(String[] args) {
 
         System.out.printf("square rectangle: %.2f \n" , rectangle.square());
