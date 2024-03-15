c6a1bb7f9c74a4176d2a667e6a1e27b1a9e0fdd2
Sergey Anisimov <s.anisimov@gmail.com>
2024-03-15 14:34:04 +0300

    Итерация 2, пункт 5 Класс Object "Все методы класса Методы hashCode и equals и контракт между ними Где используются hashCode и equals"

--- iteration2/objectHashCodeAndEquals/src/test/java/ru/itfb/model/CustomerTest.java
+++ iteration2/objectHashCodeAndEquals/src/test/java/ru/itfb/model/CustomerTest.java
@@ -35,32 +35,6 @@ class CustomerTest {
                 .fName("fName")
                 .build();
 
-        log.info(customer::toString);
-
         assertTrue(customer.toString().contains(customer.getClass().getName() + "@" + Integer.toHexString(customer.hashCode())));
     }
-
-    @Test
-    @DisplayName("Сравнение default hashCode() и переопределенного")
-    void Customer_hashCodeAndEquals() {
-
-        // 1. Если два объекта равны по методу equals(), то у них должны быть одинаковые хеш-коды.
-        //geven
-        Customer customer1 = new Customer.CustomerBuilder()
-                .id(1L)
-                .lName("lName")
-                .fName("fName")
-                .build();
-        Customer customer2 = new Customer.CustomerBuilder()
-                .id(1L)
-                .lName("lName")
-                .fName("fName")
-                .build();
-        //when
-        assertTrue(
-        //then
-                customer1.equals(customer2) && customer1.hashCode() == customer2.hashCode()
-        );
-    }
-
 }
